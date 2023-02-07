27ea8ee1f2975491006ab05a8777c2e70cdbca10
varshadhanotiya <varshadhanotiya7026@gmail.com>
2023-02-07 15:50:26 +0530

    downloadbarcode

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var a = 0;
+var a = 0;  //flag variable
 let qrcode;
 function generateqr()
 {  
@@ -68,4 +68,97 @@ function downloadImageURL()
 function downloadqr()
 {
     setTimeout(downloadImageURL(),1000);
+}
+
+
+
+//barcode
+
+var flag = 0;
+  let barcode;
+  function generatebarcode()
+  {  
+      var barinput = document.getElementById("bar-content").value;
+      
+      if(barinput == null || barinput == "")
+      {
+          alert("Enter text..");
+      }
+      else{
+          if(flag == 0)
+          {
+            //let barContentInput = document.getElementById("bar-content");
+            function barcode(){
+                data = document.querySelector('.input').value;
+                JsBarcode('#barcode' , data , {
+                  background : '#fff',
+                  color : '#000',
+                  width:1,
+                  height : 100,
+                  displayValue : false
+              });
+            }
+
+                let barCode;
+                let barGenerationForm = document.getElementById("bar-generation-form");                
+
+                // Event listener for form submit event
+                barGenerationForm.addEventListener("submit", function (event) {
+
+                // Prevent form submission
+                event.preventDefault();
+
+                //let barContent = barContentInput.value;
+                if (barCode == null) {
+                    // Generate code initially
+                    barCode = barcode();
+                } else {
+                    // If code already generated then make 
+                    // again using same object
+                    //document.getElementById('bardownload').style.display="block";
+                    barCode.makeCode(barinput);
+                }
+          });
+                flag++;
+               document.getElementById('bardownload').style.display="block";
+          }
+                else{
+                    barCode.makeCode(barinput);
+                    document.getElementById('bardownload').style.display="block";
+                }
+            }
+  }
+
+
+//Download barcode
+  function bardownload() {
+  const svgElem = document.querySelector('svg')
+  const serializer = new XMLSerializer();
+  let svgData = serializer.serializeToString(svgElem);
+  svgData = '<?xml version="1.0" standalone="no"?>\r\n' + svgData;
+  const svgBlob = new Blob([svgData], {
+    type: 'image/svg+xml;charset=utf-8',
+  });
+  let DOMURL = window.URL || window.webkitURL || window;
+  const url = DOMURL.createObjectURL(svgBlob);
+
+  const img = new Image();
+  img.onload = () => {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    const domRect = svgElem.getBBox();
+    canvas.width = domRect.width;
+    canvas.height = domRect.height;
+    ctx.drawImage(img, 0, 0, domRect.width, domRect.height);
+    DOMURL.revokeObjectURL(url);
+
+    const imgURI = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+
+    downloadURI(imgURI ,'barcode.png');
+  };
+  img.onerror = (e) => {
+    console.error('Image not loaded', e);
+  };
+
+  img.src = url;
 }
